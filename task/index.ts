0128f5c2d4e60a3d10117f1979f8912b683a5f7c
Stefan Kert <stefan.kert@sbg.at>
2019-03-19 16:47:44 +0100

    Added option to specify multiple files

--- task/index.ts
+++ task/index.ts
@@ -15,30 +15,44 @@ async function run(): Promise<void> {
     tl.setResourcePath(path.join(__dirname, "task.json"));
     secureFileId = tl.getInput("secureFileId", true);
     signCertPassword = tl.getInput("signCertPassword", true);
-    filePath = tl.getInput("filePath", true);
     timeServer = tl.getInput("timeServer", true);
     hashingAlgorithm = tl.getInput("hashingAlgorithm", true);
 
-    console.log("Downloadig secure file " + secureFileId);
-    secureFileHelpers = new sec.SecureFileDownloader();
-    secureFilePath = await secureFileHelpers.downloadSecureFile(secureFileId);
+    let filePaths: string[] = tl.getDelimitedInput("filePaths", "\n", true);
+    let rootFolder: string = tl.getPathInput("rootFolder", true, true);
 
-    console.log("Signing file");
-    var exePath: string = path.resolve(__dirname, "./signtool.exe");
-    console.log("Executing signtool at " + exePath);
-    exec.execFile(exePath, ["sign", "/fd", hashingAlgorithm, "/t", timeServer, "/f", secureFilePath, "/p", signCertPassword, filePath],
-      (err, data) => {
-        if (err) {
-          console.error("Signtool failed. Output: ");
-          console.error(err);
-          tl.setResult(tl.TaskResult.Failed, err.message);
-        } else {
-          console.log("Signtool succeeded. Output: ");
-          console.log(data);
-        }
-        console.log("Deleting securfile");
-        console.log("Job Finished: Successfully signed file " + filePath + " with certificate in " + secureFilePath);
+    rootFolder = path.normalize(rootFolder);
+
+    let allPaths: string[] = tl.find(rootFolder); // default find options (follow sym links)
+    let matchedPaths: string[] = tl.match(allPaths, filePaths, rootFolder); // default match options
+    let matchedFiles: string[] = matchedPaths.filter((itemPath: string) => !tl.stats(itemPath).isDirectory()); // filter-out directories
+
+    if (matchedFiles.length > 0) {
+      console.log("Downloadig secure file " + secureFileId);
+      secureFileHelpers = new sec.SecureFileDownloader();
+      secureFilePath = await secureFileHelpers.downloadSecureFile(secureFileId);
+
+      var exePath: string = path.resolve(__dirname, "./signtool.exe");
+      console.log("Executing signtool at " + exePath);
+
+      matchedFiles.forEach((filePath: string) => {
+        console.log("Signing file: " + filePath);
+        exec.execFile(exePath, ["sign", "/fd", hashingAlgorithm, "/t", timeServer, "/f", secureFilePath, "/p", signCertPassword, filePath],
+          (err, data) => {
+            if (err) {
+              console.error("Signtool failed. Output: ");
+              console.error(err);
+              tl.setResult(tl.TaskResult.Failed, err.message);
+            } else {
+              console.log("Signtool succeeded. Output: ");
+              console.log(data);
+            }
+            console.log("Job Finished: Successfully signed file " + filePath + " with certificate in " + secureFilePath);
+          });
       });
+    } else {
+      console.log("No files matching the pattern");
+    }
   } catch (err) {
     console.error(err);
     secureFileHelpers.deleteSecureFile();
