c1731f344ef40c78ea7e7fdb02b65689f4b350c8
Mark McGookin <mark@brightpay.co.uk>
2019-09-05 14:52:13 +0100

    added /d to test

--- tests/L0CodeSigningWithLatestVersionInstalled.ts
+++ tests/L0CodeSigningWithLatestVersionInstalled.ts
@@ -10,6 +10,7 @@ const signCertPassword: string = "PASSWORD";
 const timeServer: string = "http://timestamp.digicert.com";
 const hashingAlgorithm: string = "SHA256";
 const fileToSign: string = "DllToSign.dll";
+const description: string = "Description of dll";
 
 const signToolPath: string = "C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.17763.0\\x64\\signtool.exe";
 const certificatePath: string = path.join(__dirname, "./test-files", "TestCertificate.pfx");
@@ -19,6 +20,7 @@ tmr.setInput("signCertPassword", signCertPassword);
 tmr.setInput("timeServer", timeServer);
 tmr.setInput("hashingAlgorithm", hashingAlgorithm);
 tmr.setInput("files", filePaths);
+tmr.setInput("description", description);
 tmr.setInput("signToolLocationMethod", "latest");
 tmr.setVariableName("SECURE_FILE_PATH", certificatePath);
 
@@ -30,7 +32,7 @@ let answers: mockanswer.TaskLibAnswers = <mockanswer.TaskLibAnswers>{
     },
     exec: {}
 };
-answers.exec[`${signToolPath} sign /fd ${hashingAlgorithm} /t ${timeServer} /f ${certificatePath} /p ${signCertPassword} ${fileToSign}`] = {
+answers.exec[`${signToolPath} sign /fd ${hashingAlgorithm} /t ${timeServer} /f ${certificatePath} /p ${signCertPassword} /d ${description} ${fileToSign}`] = {
     code: 0,
     stdout: `Successfully signed ${fileToSign}`
 };
