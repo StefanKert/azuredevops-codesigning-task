2d4b8be7de9b7294dd9f5072db43d37a75b6080c
Rico Turman <rturman@isi-info.com>
2021-06-18 16:47:58 -0500

    Add logic to set filepath based on configured location

--- task/precodesigning.ts
+++ task/precodesigning.ts
@@ -7,9 +7,18 @@ async function run(): Promise<void> {
     let secureFileHelper: sec.SecureFileDownloader = new sec.SecureFileDownloader();
     try {
         tl.setResourcePath(path.join(__dirname, "task.json"));
+        
+        let secureFilePath: string;
+        let secureFileLocation: string = tl.getInput("secureFileLocation", true);
+        if (secureFileLocation == "azure") {
+            secureFileId = tl.getInput("secureFileId", true);
+            secureFilePath = await secureFileHelper.downloadSecureFile(secureFileId);
+        }
+        else if (secureFileLocation == "filepath")
+            secureFilePath = tl.getInput("secureFileLocalPath", true);
+        else
+            throw "The configured secure file location is invalid."
 
-        secureFileId = tl.getInput("secureFileId", true);
-        let secureFilePath: string = await secureFileHelper.downloadSecureFile(secureFileId);
         tl.setTaskVariable("SECURE_FILE_PATH", secureFilePath);
     } catch (err) {
         tl.setResult(tl.TaskResult.Failed, err);
